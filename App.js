575d4bee726bf9d4ba94da70ce08b61382386756
sweety ashtikar <101049802+sweetyashtikar@users.noreply.github.com>
2022-10-13 16:19:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import './App.css'
+import Homepage from "./components/Homepage/Homepage"
+import Login from "./components/Login/Login"
+import Register from "./components/Register/Register"
+
+function App() {
+
+  return (
+    <div className="App">
+      {/*< Homepage/>*/}
+      {/*  <Login />*/}
+      <Register />
+    </div>
+  );
+}
+
+export default App;
